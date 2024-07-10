91051a22a97bf8290e2be730c5d3aa3732e0ce17
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2024-07-09 23:53:03 -0500

    settings

--- src/main/java/com/linktic/service/order/OrderApplication.java
+++ src/main/java/com/linktic/service/order/OrderApplication.java
@@ -1,9 +1,11 @@
 package com.linktic.service.order;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDynamoDBRepositories(basePackages = "com.linktic.service.order.repository")
 public class OrderApplication {
 
 	public static void main(String[] args) {
